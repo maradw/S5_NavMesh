7c185e645fcbcb784149ebfd7248913ceae7d753
maradw <maria.mendoza@tecsup.edu.pe>
2025-04-16 12:02:19 -0500

    path

--- Assets/Scripts/navMeshMovement.cs
+++ Assets/Scripts/navMeshMovement.cs
@@ -8,26 +8,24 @@ public class navMeshMovement : MonoBehaviour
     [SerializeField] private NavMeshAgent enemy;
     [SerializeField] private float range;
     [SerializeField] private Transform centerPlane;
+    Vector3 target;
+    private bool hasTarget = false;
 
-    Vector3 targetPos;
-
-    [SerializeField] private Transform targetPoint;
     void Start()
     {
-        
-    }
 
+    }
 
     void Update()
     {
-        MoveToTarge(targetPos);
+        CalculatePath(target);
     }
-    //
-    private bool RandomPoint(Vector3 center, float range, out Vector3 result )
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        Vector3 randomTarget = center + Random.insideUnitSphere *range;
+        Vector3 randomTarget = center + Random.insideUnitSphere * range;
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomTarget, out hit,0.5f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(randomTarget, out hit, 0.5f, NavMesh.AllAreas))
         {
             result = hit.position;
             return true;
@@ -35,30 +33,52 @@ public class navMeshMovement : MonoBehaviour
         result = Vector3.zero;
         return false;
     }
-    void MoveToTargePosition()
+
+    void CallRandom(Vector3 target)
     {
-        if (enemy.remainingDistance <= enemy.stoppingDistance)
+
+        if (enemy.remainingDistance <= enemy.stoppingDistance && !enemy.pathPending)
         {
-            Vector3 target;
             if (RandomPoint(centerPlane.position, range, out target))
             {
                 Debug.DrawRay(target, Vector2.up, Color.magenta, 0.8f);
                 enemy.SetDestination(target);
-                
             }
         }
-    }
-    void MoveToTarge(Vector3 target)
-    {
-        if (enemy.remainingDistance <= enemy.stoppingDistance)
-            if (RandomPoint(centerPlane.position, range, out target))
-            {
-                Debug.DrawRay(target, Vector2.up, Color.magenta, 0.8f);
-                enemy.SetDestination(target);
 
-            }
-        
+
     }
-    ///
+    void CalculatePath(Vector3 targetPosition)
+    {
+
+        if (!enemy.pathPending && enemy.remainingDistance <= enemy.stoppingDistance)
+        {
+            hasTarget = false;
+        }
+        if (!hasTarget)
+        {
+            
+            if (RandomPoint(centerPlane.position, range, out targetPosition))
+            {
+                NavMeshPath path = new NavMeshPath();
+
+                if (enemy.CalculatePath(targetPosition, path) && path.status == NavMeshPathStatus.PathComplete)
+                {
+                    target = targetPosition;
+                    enemy.SetPath(path);
+                    hasTarget = true;
+
+                    Debug.DrawRay(target, Vector3.up * 2f, Color.magenta, 1f);
+                }
+                else
+                {
+                    Debug.Log("Camino no válido hacia: " + targetPosition);
+                }
+            }
+        }
+
+    }
+
+
 
 }
